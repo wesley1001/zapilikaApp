ea6c538ebf0b2b3f0f06baf69ed3fda9a8f2f038
Anton Puko <kindmoney@yandex.ru>
2016-03-17 13:56:22 +0200

    -add navigator to main component -init SearchView component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,37 @@
 import React, {
   Component,
   View,
-  Text
+  Text,
+  StyleSheet,
+  Navigator,
+
 } from 'react-native';
 
+import ROUTES from '../routes';
+
 export default class Main extends Component {
+  renderScene(route, navigator) {
+    var Component = ROUTES[route.name];
+    return <Component route={route} navigator={navigator}/>
+  }
+
   render() {
     return (
-      <View>
-        <Text>
-          Zapilika app!!
-        </Text>
+      <View style={styles.container}>
+        <Navigator
+          initialRoute={{name: 'searchView'}}
+          renderScene={this.renderScene}
+          configureScene={() =>
+         {return Navigator.SceneConfigs.FloatFromBottomAndroid;}}
+        />
       </View>
     )
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
---
+++ src/components/SearchView.js
@@ -0,0 +1,19 @@
+'use strict';
+import React, {
+  Component,
+  View,
+  Text
+} from 'react-native';
+
+
+export default class SearchView extends Component {
+  render() {
+    return(
+      <View>
+        <Text>
+          Hello this is search view and your a on the right way you!
+        </Text>
+      </View>
+    )
+  }
+}
